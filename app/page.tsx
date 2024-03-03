a59eab3eee16cc67419407aa8a604e37fd26a890
sdgsnehal <sdgsnehal@gmail.com>
2024-03-03 11:41:48 +0530

    added hero carousel

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,47 @@
-import React from 'react'
+import HeroCarousel from "@/components/HeroCarousel";
+import Searchbar from "@/components/Searchbar";
+import Image from "next/image";
+import React from "react";
 
 const Home = () => {
   return (
-    <div>page</div>
-  )
-}
+    <>
+      <section className="px-6 md:px-20 py-24">
+        <div className="flex max-xl:flex-col gap-16">
+          <div className="flex flex-col justify-center">
+            <p className="small-text">
+              smart shopping starts here
+              <Image
+                src="/assets/icons/arrow-right.svg"
+                alt="arrow-right"
+                width={16}
+                height={16}
+              />
+            </p>
+            <h1 className="head-text">
+              Unleash the power of
+              <span className="text-primary"> PriceVise</span>
+            </h1>
 
-export default Home
+            <p className="mt-6">
+              Powerful,self-serve product and growth analytics to help you
+              convert,engage,and retain more
+            </p>
+            <Searchbar />
+          </div>
+          <HeroCarousel />
+        </div>
+      </section>
+      <section className="trending-section">
+        <h2 className="section-text">Trending</h2>
+        <div className="flex flex-wrap gap-x-8 gap-y-16">
+          {["apple", "book", "glass"].map((item) => (
+            <div>{item}</div>
+          ))}
+        </div>
+      </section>
+    </>
+  );
+};
+
+export default Home;
