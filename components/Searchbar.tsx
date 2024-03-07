dfd0ab6aa5591db4f449bbcb06c8d3cb992da91f
sdgsnehal <sdgsnehal@gmail.com>
2024-03-07 12:44:41 +0530

    fix(Searchbar.tsx): add form validation to check if the entered link is a valid Amazon product link feat(Searchbar.tsx): add loading state and disable search button when input is empty or during loading state

--- components/Searchbar.tsx
+++ components/Searchbar.tsx
@@ -1,8 +1,40 @@
 "use client";
-import React from "react";
+
+import React, { FormEvent, useState } from "react";
+const isValidAmazonProductUrl = (url: string) => {
+  try {
+    const parsedURL = new URL(url);
+    const hostname = parsedURL.hostname;
+    if (
+      hostname.includes("amazon.in") ||
+      hostname.includes("amazon.com") ||
+      hostname.endsWith("amazon")
+    ) {
+      return true;
+    }
+  } catch {
+    return false;
+  }
+};
 
 const Searchbar = () => {
-  const handleSubmit = () => {};
+  const [searchPrompt, setSearchPrompt] = useState("");
+  const [isLoading, setIsLoading] = useState(false);
+  const handleSubmit = (event: FormEvent<HTMLFormElement>) => {
+    event.preventDefault();
+    const isValidLink = isValidAmazonProductUrl(searchPrompt);
+    if (!isValidLink) {
+      return alert("Please enter a valid Amazon product link");
+    }
+    try {
+      setIsLoading(true);
+      // scrap the product
+    } catch {
+      console.log("error");
+    } finally {
+      setIsLoading(false);
+    }
+  };
   return (
     <form
       className="flex flex-wrap gap-4 mt-12"
@@ -13,8 +45,16 @@ const Searchbar = () => {
         type="text"
         placeholder="Enter product link"
         className="searchbar-input"
+        value={searchPrompt}
+        onChange={(e) => setSearchPrompt(e.target.value)}
       />
-      <button type="submit" className="searchbar-btn">Search</button>
+      <button
+        type="submit"
+        className="searchbar-btn"
+        disabled={searchPrompt === ""}
+      >
+        {isLoading ? "Searching..." : "Search"}
+      </button>
     </form>
   );
 };
