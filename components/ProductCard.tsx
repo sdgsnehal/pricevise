364e8a06f6b88728d38cd9f05b6900546d433bcd
sdgsnehal <sdgsnehal@gmail.com>
2024-03-14 16:10:02 +0530

    feat(page.tsx): add support for fetching product details by ID and displaying them on the page fix(page.tsx): redirect to homepage if product is not found feat(ProductCard.tsx): display product title, category, and price on the product card

--- components/ProductCard.tsx
+++ components/ProductCard.tsx
@@ -19,6 +19,18 @@ const ProductCard = ({product}:Props) => {
             height={200}
             className="product-card_img"/>
         </div>
+        <div className='flex flex-col gap-3'>
+            <h3 className='product-title'>{product.title}</h3>
+            <div className='flex justify-between'>
+                <p className='text-black opacity-50 text-lg capitalize '>
+                   {product.category} 
+                </p>
+                <p className='text-black text-lg font-semibold'>
+                    <span>{product?.currency}</span>
+                    <span>{product?.currentPrice}</span>
+                </p>
+            </div>
+        </div>
     </Link>
   )
 }
