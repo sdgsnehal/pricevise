2e078867e033f13a96620eabbba937eca1e15af6
sdgsnehal <sdgsnehal@gmail.com>
2024-04-22 22:44:24 +0530

    feat(Modal.tsx): add email state, isSubmitting state, and handleSubmit function to handle form submission in the modal component

--- components/Modal.tsx
+++ components/Modal.tsx
@@ -2,9 +2,13 @@
 import React, { Fragment } from "react";
 import { useState } from "react";
 import { Dialog, Transition } from "@headlessui/react";
+import Image from "next/image";
 
 const Modal = () => {
   let [isOpen, setIsOpen] = useState(true);
+  const [isSubmitting, setIsSubmitting] = useState(false);
+  const [email, setEmail] = useState("");
+  const handleSubmit = async () => {};
   const openModal = () => setIsOpen(true);
   const closeModal = () => setIsOpen(false);
   return (
@@ -26,7 +30,77 @@ const Modal = () => {
             >
               <Dialog.Overlay className="fixed inset-0" />
             </Transition.Child>
-            <Transition.Child></Transition.Child>
+            <span
+              className="inline-block h-screen align-middle"
+              aria-hidden="true"
+            ></span>
+            <Transition.Child
+              as={Fragment}
+              enter="ease-out duration-300"
+              enterFrom="opacity-0 scale-95"
+              enterTo="opacity-100 scale-100"
+              leave="ease-in duration-200"
+              leaveFrom="opacity-100 scale-100"
+              leaveTo="opacity-0 scale-95"
+            >
+              <div className="dialog-content">
+                <div className="flex flex-col">
+                  <div className="flex  justify-between">
+                    <div className="p-3 border border-gray-200 rounded-10">
+                      <Image
+                        src="/assets/icons/logo.svg"
+                        alt="logo"
+                        width={28}
+                        height={28}
+                      />
+                    </div>
+                    <Image
+                      src="/assets/icons/x-close.svg"
+                      alt="close"
+                      width={24}
+                      height={24}
+                      className="cursor-pointer"
+                      onClick={closeModal}
+                    />
+                  </div>
+
+                  <h4 className="dialog-head_text">
+                    stay updated with product pricing alerts rights in your
+                    inbox
+                  </h4>
+                  <p className="text-sm text-gray-600 mt-2">
+                    Never miss a bargian again with our alerts
+                  </p>
+                </div>
+                <form className="flex flex-col mt-5" onSubmit={handleSubmit}>
+                  <label
+                    htmlFor="email"
+                    className="text-sm font-medium text-gray-700"
+                  >
+                    Email Address
+                  </label>
+                  <div className="dialog-input_container">
+                    <Image
+                      src="/assets/icons/mail.svg"
+                      alt="mail"
+                      width={18}
+                      height={18}
+                    />
+                    <input
+                      type="email"
+                      id="email"
+                      value={email}
+                      onChange={(e) => setEmail(e.target.value)}
+                      className="dialog-input"
+                      placeholder="Enter your email address"
+                    />
+                  </div>
+                  <button type="submit" className="dialog-btn">
+                    {isSubmitting ? "Submitting.." : "Track"}
+                  </button>
+                </form>
+              </div>
+            </Transition.Child>
           </div>
         </Dialog>
       </Transition>
