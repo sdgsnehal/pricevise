4ada91276d63ef84c64466950f1adaf12defd232
sdgsnehal <sdgsnehal@gmail.com>
2024-04-28 20:27:32 +0530

    feat(route.ts): add new route for cron job to fetch and update product data from Amazon feat(actions/index.ts): enable calculation and storage of average price for scraped products refactor(scraper/index.ts): refactor scrapeAmazonProduct function to improve readability and maintainability style(utils.ts): update Notification enum and formatNumber function for consistency and readability

--- lib/actions/index.ts
+++ lib/actions/index.ts
@@ -28,7 +28,7 @@ export async function scrapeAndStoreProduct(productUrl: string) {
         priceHistory: updatedPriceHistory,
         lowestPrice: getLowestPrice(updatedPriceHistory),
         highestPrice: getHighestPrice(updatedPriceHistory),
-        //averagePrice: getAveragePrice(updatedPriceHistory),
+        averagePrice: getAveragePrice(updatedPriceHistory),
       };
     }
     const newProduct = await Product.findOneAndUpdate(
